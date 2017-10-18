13faa1a2e0f59bb302b04ea3be154b2c7f7442b0
tS7hKamAYL84j91 <jim@jimbob-air.local>
2017-10-18 11:50:45 +0100

    aligned the priv/static directory with brunch process

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -20,7 +20,7 @@ exports.config = {
       // }
     },
     stylesheets: {
-      joinTo: "css/app.css"
+      joinTo: "css/app.css", order: {after: ["chrono.css"]}
     },
     templates: {
       joinTo: "js/app.js"
