d07e7888a5624994a8b4a942435cfda9e1cadb3c
tS7hKamAYL84j91 <jim@jimbob-air.local>
2017-11-06 21:55:25 +0000

    containers running in K8 and Openshift

--- config/prod.exs
+++ config/prod.exs
@@ -2,12 +2,14 @@ use Mix.Config
 
 config :chrono, ChronoWeb.Endpoint,
   http: [port: {:system, "PORT"}],
+  url: [host: "${HOST}", port: "${PORT}"],
+  secret_key_base: "${SECRET_KEY_BASE}",
   check_origin: false,
   server: true
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :debug
 
 config :chrono, Chrono.Contentful.Repo,
-  contentful_key: {:system, "CONTENTFUL_KEY"},
-  contentful_space: {:system, "CONTENTFUL_SPACE"}
+  contentful_key: "${CONTENTFUL_KEY}",
+  contentful_space: "${CONTENTFUL_SPACE}"
