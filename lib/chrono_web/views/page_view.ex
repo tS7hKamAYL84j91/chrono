fc25fa12ae059b00c712063a95d99031af3f1e70
tS7hKamAYL84j91 <jimandsalbrown@gmail.com>
2018-01-05 23:36:57 +0000

    Added images from contentful to font page

--- lib/chrono_web/views/page_view.ex
+++ lib/chrono_web/views/page_view.ex
@@ -8,25 +8,37 @@ defmodule ChronoWeb.PageView do
   defp parse_content(cont) do 
     cont 
     |> Map.put(:html, cont.body |> parse_html) 
-    |> Map.put(:template, cont |> get_template )
-    |> Map.take([:id, :title, :html, :background_img, :template])
+    |> Map.put(:template, cont |> apply_template)
+    |> Map.put(:background_img, cont |> format_photo)
+    |> Map.put(:linked_content, cont |> linked_content)
+    |> Map.take([:id, :title, :html, :background_img, :template, :linked_content])
   end
-
+ 
   defp parse_html(body) do
-    with {:ok, html, _} <- body |> Earmark.as_html do 
+    with {:ok, {:ok, html, _}} <- body |> Earmark.as_html |> Chrono.Either.either do 
       html 
     else 
-      _e -> "<p>Danger Will Robinson ... error |> no content</p>"
+      _e -> 
+        Logger.warn "#{__MODULE__}: Danger Will Robinson no html defined #{body}"
+        "<p>Danger Will Robinson ... error |> no content</p>"
     end 
   end
 
-  defp get_template(page), do: page |> Map.from_struct |> get_in([:fields, "template"]) |> default_template(page) 
+  defp apply_template(%{:sys => %{"contentType" => %{"sys" => %{"id" => "watch"}}}}), do: "showcase_img"
+  defp apply_template(cont), do: cont |> Map.from_struct |> get_in([:fields, "template"]) |> default_template(cont) 
   
-  defp default_template(nil, page) do 
-    Logger.warn "#{__MODULE__}: Danger Will Robinson no templated defined #{page.title}"
+  defp default_template(nil, cont) do 
+    Logger.warn "#{__MODULE__}: Danger Will Robinson no templated defined #{cont.title}"
     "page"
   end
   defp default_template(t, _), do: t
 
+  defp format_photo(%{:sys => %{"contentType" => %{"sys" => %{"id" => "watch"}}}}=cont), do: cont |> Map.from_struct |> get_in([:fields, "photo", "fields", "file", "url"])
+  defp format_photo(cont), do: cont.background_img
+
+  def linked_content(%Chrono.CMS.Content{linked_content: nil}), do: nil
+  def linked_content(%Chrono.CMS.Content{linked_content: cs}), do: cs |> parse_pages
+
+  
 
 end
